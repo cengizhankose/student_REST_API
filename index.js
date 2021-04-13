a841f76376d75db7f795c43d7a5ebe1125779318
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-14 00:04:59 +0300

    swagger added

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const PORT = 8080;
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Student API",
+      description: "Student API",
+      contact: {
+        name: "Cengizhan KÖSE",
+      },
+      servers: ["http://localhost:8080"],
+    },
+  },
+  apis: ["./index.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json()); // JSON parse middleware
 
@@ -9,16 +28,82 @@ app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`);
 });
 
+// Routes
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    summary: "default endpoint"
+ *    description: Main screen endpoint this endpoint doesn't do much
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 app.get("/", (req, res) => {
   res.status(200).send("Student REST API");
 });
 
+/**
+ * @swagger
+ * /student:
+ *  get:
+ *    tags:
+ *    - "student"
+ *    summary: "Get all student data"
+ *    description: Use to request all students
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 app.get("/student", (req, res) => {
   let rawdata = fs.readFileSync("./db.json");
   let data = JSON.parse(rawdata);
   res.status(200).send(data);
 });
 
+/**
+ * @swagger
+ * /student/{id}:
+ *  post:
+ *     tags:
+ *     - "student"
+ *     summary: "Add a new student"
+ *     description: "Use to add a new student."
+ *     operationId: "AddStudent"
+ *     produces:
+ *     - "application/json"
+ *     parameters:
+ *     - name: "id"
+ *       in: "path"
+ *       description: "id of the student that you want to update"
+ *       required: true
+ *       type: "string"
+ *     - in: "body"
+ *       name: "body"
+ *       description: "Add a new student object"
+ *       required: true
+ *       schema:
+ *         type: "object"
+ *         properties:
+ *           name:
+ *             type: "string"
+ *           surname:
+ *             type: "string"
+ *           classes:
+ *             type: "array"
+ *             items:
+ *               type: "string"
+ *           school:
+ *             type: "string"
+ *     responses:
+ *       "200":
+ *         description: "Successfully added user"
+ *       "400":
+ *         description: "Missing body value"
+ *       "404":
+ *         description: "invalid id"
+ */
 app.post("/student/:id", (req, res) => {
   const { id } = req.params;
   const { name, surname, classes, school } = req.body;
@@ -39,15 +124,46 @@ app.post("/student/:id", (req, res) => {
   let data = JSON.parse(rawdata);
 
   getStudent(data, id)
-    ? res.status(400).send({ message: "Change your id and try again" })
+    ? res.status(404).send({ message: "Change your id and try again" })
     : data.push(student);
 
   fs.writeFileSync("./db.json", JSON.stringify(data));
-  qq;
-  res.send({ student: `Welcome ${name} ID:${id}` });
+  res
+    .status(201)
+    .send({
+      student: `Student named ${name} successfully added with ID:${id}`,
+    });
 });
 
-app.delete("/student/:id", function (req, res) {
+/**
+ * @swagger
+ * /student/{id}:
+ *  delete:
+ *     tags:
+ *     - "student"
+ *     summary: "Delete student"
+ *     description: "Use to delete a student."
+ *     operationId: "deleteStudent"
+ *     produces:
+ *     - "application/json"
+ *     parameters:
+ *     - name: "id"
+ *       in: "path"
+ *       description: "id of the student that you want to delete"
+ *       required: true
+ *       type: "string"
+ *     responses:
+ *       "204":
+ *         description: "Successfully deleted user"
+ *       "400":
+ *         description: "Missing id"
+ *       "404":
+ *         description: "There is no equal match to entered id"
+ */
+
+//TODO HTTP response "400","404":
+
+app.delete("/student/:id", (req, res) => {
   const { id } = req.params;
 
   let rawdata = fs.readFileSync("./db.json");
@@ -60,16 +176,58 @@ app.delete("/student/:id", function (req, res) {
   });
   console.log(data);
   fs.writeFileSync("./db.json", JSON.stringify(data));
-  res.send({
-    message: `student succesfully deleted ID:${id}`,
+  res.status(204).send({
+    message: `student successfully deleted ID:${id}`,
   });
 });
 
-app.put("/student/updateStudent/:id", function (req, res) {
+/**
+ * @swagger
+ * /student/{id}:
+ *  put:
+ *     tags:
+ *     - "student"
+ *     summary: "Update student"
+ *     description: "Use to update a student."
+ *     operationId: "updateStudent"
+ *     produces:
+ *     - "application/json"
+ *     parameters:
+ *     - name: "id"
+ *       in: "path"
+ *       description: "id of the student that you want to update"
+ *       required: true
+ *       type: "string"
+ *     - in: "body"
+ *       name: "body"
+ *       description: "Updated student object"
+ *       required: true
+ *       schema:
+ *         type: "object"
+ *         properties:
+ *           name:
+ *             type: "string"
+ *           surname:
+ *             type: "string"
+ *           classes:
+ *             type: "array"
+ *             items:
+ *               type: "string"
+ *           school:
+ *             type: "string"
+ *     responses:
+ *       "200":
+ *         description: "Successfully updated user"
+ *       "400":
+ *         description: "Missing body value"
+ *       "404":
+ *         description: "There is no equal match to entered id"
+ */
+app.put("/student/:id", (req, res) => {
   const { id } = req.params;
-  const { classToAdd, name, surname, school } = req.body;
+  const { classes, name, surname, school } = req.body;
 
-  if (!name || !surname || !classToAdd || !school) {
+  if (!name || !surname || !classes || !school) {
     res.status(400).send({ message: "Missing body value" });
   }
   const newStudent = {
@@ -83,19 +241,15 @@ app.put("/student/updateStudent/:id", function (req, res) {
 
   let student = getStudent(data, id);
 
-  console.log("1.-----------", student);
-
   if (student) {
     student.name = newStudent.name;
     student.surname = newStudent.surname;
     student.school = newStudent.school;
-    student.classes.push(classToAdd);
-    console.log("2.-----------", student);
-
+    student.classes = newStudent.classes;
     fs.writeFileSync("./db.json", JSON.stringify(data));
-    res.send({ message: `${classToAdd} class succesfully added to ID:${id}` });
+    res.status(200).send({ message: `ID:${id} student updated successfully` });
   } else {
-    res.status(400).send({ message: "No matching students" });
+    res.status(404).send({ message: "No matching students" });
   }
 });
 
